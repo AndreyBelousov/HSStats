ef9bb40e27c227a7d6f16ba0a40f85d759b1d794
Andrey Belousov <lriddickl1988@mail.ru>
2015-03-27 12:02:30 +0200

    Total Stats

--- HSStats/Controllers/StatsController.cs
+++ HSStats/Controllers/StatsController.cs
@@ -15,10 +15,28 @@ namespace HSStats.Controllers
         // GET: /Stats/
         public ActionResult Index()
         {
-            ViewBag.TotalWins = db.Matches.Where(m => m.Result == Results.Win).Count();
-            ViewBag.TotalDefeats = db.Matches.Where(m => m.Result == Results.Defeat).Count();
-            ViewBag.TotalDraws = db.Matches.Where(m => m.Result == Results.Draw).Count();
-            return View();
+            StatsViewModel statsVM = new StatsViewModel();
+            statsVM.TotalMatches = db.Matches.Count();
+            statsVM.TotalWins = db.Matches.Where(m => m.Result == Results.Win).Count();
+            statsVM.TotalDefeats = db.Matches.Where(m => m.Result == Results.Defeat).Count();
+            statsVM.TotalDraws = db.Matches.Where(m => m.Result == Results.Draw).Count();
+
+            statsVM.HeroesMatches = new int[Enum.GetNames(typeof(Heroes)).Length];
+            statsVM.HeroesWins = new int[Enum.GetNames(typeof(Heroes)).Length];
+            statsVM.HeroesDefeats = new int[Enum.GetNames(typeof(Heroes)).Length];
+            statsVM.WinPercentages = new double[Enum.GetNames(typeof(Heroes)).Length];
+            
+            for ( int i = 0; i < Enum.GetNames(typeof(Heroes)).Length; i++ )
+            {
+                statsVM.HeroesMatches[i] = db.Matches.Where(m => m.MyHero == (Heroes)i).Count();
+                statsVM.HeroesWins[i] = db.Matches.Where(m => m.MyHero == (Heroes)i && m.Result == Results.Win).Count();
+                statsVM.HeroesDefeats[i] = db.Matches.Where(m => m.MyHero == (Heroes)i && m.Result == Results.Defeat).Count();
+                if ( statsVM.HeroesWins[i] != 0 )
+                {
+                    statsVM.WinPercentages[i] = Math.Round((double)statsVM.HeroesWins[i] / ( statsVM.HeroesWins[i] + statsVM.HeroesDefeats[i] ) * 100, 2);
+                }
+            }
+            return View(statsVM);
         }
 	}
 }
